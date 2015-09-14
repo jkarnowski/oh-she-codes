4f35f7b40f22d46095bc3f5e13e5579011f8660a
jkarnowski <jaclyn.karnowski@gmail.com>
2015-09-13 17:49:22 -0700

    update routes for project and add js for posts

--- oh-she-codes/config/routes.rb
+++ oh-she-codes/config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'projects/index'
-
-  get 'projects/new'
-
-  get 'projects/show'
-
-  get 'projects/edit'
-
   resources :posts
+  resources :projects
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
