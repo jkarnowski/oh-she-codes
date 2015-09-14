b238e2b4b984ed491acf5d816ce3218d4ac0fd6f
jkarnowski <jaclyn.karnowski@gmail.com>
2015-09-13 17:47:25 -0700

    add Project model and controller

--- oh-she-codes/config/routes.rb
+++ oh-she-codes/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'projects/index'
+
+  get 'projects/new'
+
+  get 'projects/show'
+
+  get 'projects/edit'
+
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
